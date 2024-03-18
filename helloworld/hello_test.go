e77a23ccdba9cded29d6741baa2233794e6efc03
Prakash Prasad <prakash.prasad@telenor.no>
2024-03-18 21:58:11 +0100

    go with tests for integers

--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -50,9 +50,9 @@ func TestHello(t *testing.T) {
 	})
 }
 
-func assertCorrectMessage(t *testing.T, got, want string) {
-	t.Helper()
+func assertCorrectMessage(tb *testing.TB, got, want string) {
+	tb.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		tb.Errorf("got %q want %q", got, want)
 	}
 }
