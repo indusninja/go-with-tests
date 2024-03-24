93c65fa3da5dbe921a7e7638e61ed17b9e1e6d86
Prakash Prasad <prakash.prasad@telenor.no>
2024-03-24 15:23:25 +0100

    exercise 3.2: iterations

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -4,6 +4,9 @@ const englishHelloExpression = "Hello"
 const spanishHelloExpression = "Hola"
 const frenchHelloExpression = "Bonjour"
 
+// Hello returns a personalised greeting, in a given language.
+// If no name is provided, the greeting will default to the "World".
+// If no language (or an unsupported language) is provided, the greeting will default to English.
 func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
@@ -12,6 +15,9 @@ func Hello(name, language string) string {
 	return GetHelloExpressionInLanguage(language) + ", " + name
 }
 
+// GetHelloExpressionInLanguage returns the hello expression in a given language.
+// It currently supports English, Spanish and French.
+// By default, the expression provided is in English.
 func GetHelloExpressionInLanguage(language string) string {
 	prefix := englishHelloExpression
 	switch language {
--- integers/addr.go
+++ integers/addr.go
@@ -1,5 +1,6 @@
 package integers
 
-func Addr(x, y int) int {
+// Add takes two integers and returns the sum of them.
+func Add(x, y int) int {
 	return x + y
 }
