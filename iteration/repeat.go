321bb3b2276ada68f7a446661fdc2b7bd909bee1
Prakash Prasad <prakash.prasad@telenor.no>
2024-03-27 15:44:39 +0100

    exercise 3.3: iterations

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,5 +1,6 @@
 package iteration
 
+// Repeat takes a character and repeats it number of times as specified by repeatCountcd iter
 func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,10 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	t.Run("repeat 'a' 5 times", func(t *testing.T) {
@@ -11,4 +15,22 @@ func TestRepeat(t *testing.T) {
 			t.Errorf("expected %q but got %q", expected, repeated)
 		}
 	})
+}
+
+func BenchmarkRepeatDumb(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func BenchmarkRepeatStringPkg(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		strings.Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("b", 3)
+	fmt.Println(repeated)
+	// Output: bbb
 }
