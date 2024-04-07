6b509ade3f2664d0276a95467ac8e60a60f89c5d
Prakash Prasad <prakash.prasad@telenor.no>
2024-04-07 23:13:02 +0200

    exercise 5.2: structs

--- structs/shapes.go
+++ structs/shapes.go
@@ -2,6 +2,10 @@ package structs
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -9,6 +13,11 @@ type Rectangle struct {
 
 type Circle struct {
 	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
 }
 
 func (r Rectangle) Perimeter() float64 {
@@ -21,4 +30,8 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * 0.5
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,26 +3,39 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
+
+	verification := func(t testing.TB, got, want float64) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
 	t.Run("perimeter of a rectangle with sides - 10 and 10", func(t *testing.T) {
 		rectangle := Rectangle{10.0, 10.0}
-		assertCorrectMessage(t, rectangle.Perimeter(), 40.0)
+		verification(t, rectangle.Perimeter(), 40.0)
 	})
 }
 
 func TestArea(t *testing.T) {
-	t.Run("area of a rectangle with sides - 6 and 10", func(t *testing.T) {
-		rectangle := Rectangle{6.0, 10.0}
-		assertCorrectMessage(t, rectangle.Area(), 60.0)
-	})
-	t.Run("area of a circle with radius - 10", func(t *testing.T) {
-		circle := Circle{10.0}
-		assertCorrectMessage(t, circle.Area(), 314.1592653589793)
-	})
-}
 
-func assertCorrectMessage(t *testing.T, got, want float64) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %g want %g", got, want)
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 6, Height: 10}, hasArea: 60.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+
 }
