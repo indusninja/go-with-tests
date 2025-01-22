5b481aa9f572bbbd749eca4f783a6f418258f69d
Prakash Prasad <prakash.prasad@telenor.no>
2025-01-22 14:31:38 +0100

    Finished Select sub-section

--- 11-select/racer_test.go
+++ 11-select/racer_test.go
@@ -1,15 +1,57 @@
 package _1_select
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
 func TestRacer(t *testing.T) {
-	slowUrl := "http://www.facebook.com"
-	fastUrl := "http://www.quii.dev"
 
-	want := fastUrl
-	got := Racer(slowUrl, fastUrl)
+	t.Run("test facebook vs google", func(t *testing.T) {
+		slowUrl := "http://www.facebook.com"
+		fastUrl := "http://www.google.com"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		want := fastUrl
+		got, _ := Racer(slowUrl, fastUrl)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("test fake fast vs fake slow server", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("timeout", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Errorf("expected an error but didn't get one")
+		}
+	})
 }
